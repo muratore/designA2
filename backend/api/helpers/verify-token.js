b3cf02082186c407ec6afdcc4889edeec8e9562c
Alessandro Muratore <muratore@gmail.com>
2025-05-06 15:08:10 -0300

    tirar funções

--- backend/api/helpers/verify-token.js
+++ backend/api/helpers/verify-token.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 const mysecret = process.env.JWT_SECRET;
 const verifyToken = (req, res, next) => {
   const auth = req.headers.authorization;
-  console.log(`----- ${auth}  ----- `);
 
   if (!auth) {
     res.status(401).json({ message: "Acesso Negado!" });
