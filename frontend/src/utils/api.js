c013d42b491a965956aaaefcc05d9e84915e142c
Alessandro Muratore <muratore@gmail.com>
2025-05-13 05:38:40 -0300

    front

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
  const api = axios.create({
-    baseURL: "http://localhost:3005" // Replace with your API's base URL
+    baseURL: "http://localhost:3000" // Replace with your API's base URL
     // You can add other defaults here, like headers for authentication
     // headers: { 'Authorization': 'Bearer your_token' }, 
 })
