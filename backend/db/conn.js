9d9e1869490540cb1c8cb6a22b1659ffe003f7c6
Alessandro Muratore <muratore@gmail.com>
2025-05-13 05:53:00 -0300

    uri to string

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -6,7 +6,7 @@ const uri = process.env.MONGODB_URI;
 
 const connection = async () => {
   try {
-    await mongoose.connect(uri, {});
+    await mongoose.connect(`${uri}`, {});
     console.log("Connected to MongoDB Atlas");
   } catch (error) {
     console.error("Error connecting to MongoDB Atlas: ", error);
